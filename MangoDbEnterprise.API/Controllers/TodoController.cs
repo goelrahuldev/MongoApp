b7f9b885cb8e7aec96115841e84ed4b8be32ec4e
Rahul Goel <rahul.g2510@gmail.com>
2015-07-19 02:25:51 +0530

    Swagger chages and parameter test

--- MangoDbEnterprise.API/Controllers/TodoController.cs
+++ MangoDbEnterprise.API/Controllers/TodoController.cs
@@ -4,23 +4,39 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Description;
 using MangoDbEnterprise.API.Helper;
 using MangoDbEnterprise.Domain.Entities.Todo;
 using MangoDbEnterprise.Repositories;
+using Swashbuckle.Swagger;
 
 namespace MangoDbEnterprise.API.Controllers
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class TodoController : BaseController
     {
         private readonly ITodoService _todoService;
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="referenceService"></param>
+        /// <param name="todoService"></param>
         public TodoController(IReferenceService referenceService ,ITodoService todoService) 
             : base(referenceService)
         {
             _todoService = todoService;
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
-        public ApiResult<Todo[]> Get()
+        [Route("api/tasks")]
+        [ResponseType(typeof(List<Todo>))]
+        public ApiResult<Todo[]> Get(string required, string title = ApiConstants.DefaultEmptyStringValue)
         {
             return this.CreateResponse(_todoService.Select(x => x).ToArray());
         }
